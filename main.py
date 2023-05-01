cdb22406fa7d07461cd6304c429d487d96fd32bb
Adrian Olczak <adrianolczakdev@gmail.com>
2023-05-01 21:31:55 +0200

    Clear main

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from algorithms.BFS import BFS
 
 
 if __name__ == '__main__':
-    # game = Maze(board_size=(40, 30), pathfinding_algorithm=BFS)
     # game = Maze(board=boards[3], pathfinding_algorithm=BFS)
-    game = Maze(board_size=(50, 50), pathfinding_algorithm=BFS, tick=0)
-    # game = Maze(board_size=(384, 216), pathfinding_algorithm=BFS)
+    # game = Maze(board_size=(40, 40), tick=10)
+    game = Maze(board_size=(240, 135), pathfinding_algorithm=BFS)
     game.run_game()
-
-    # bfs = BFS(game)
-    # bfs.find()
-    
